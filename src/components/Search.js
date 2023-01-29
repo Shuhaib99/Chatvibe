8a320e62e0b9a924692801835ea50730d36cc23a
shuhaib <shshuaib99@gmail.com>
2023-01-29 15:30:47 +0530

    upds ui

--- src/components/Search.js
+++ src/components/Search.js
@@ -25,44 +25,48 @@ function Search() {
     }, [search])
 
     return (
-        <div className='fixed z-50  w-full top-0  '>
+        <div className='fixed z-50  w-full top-0'>
 
             <Card>
-                <div className='ml-11 relative flex'>
+                <div className='ml-11 flex'>
                     <div className=''>
                         <Logo />
                     </div>
-                    <div className='w-full'>
-                        <center>
-                            <input value={search} onChange={(e) => {
-                                setIspop(true)
-                                setSearch(e.target.value)
-                            }} className='p-2 w-11/12' placeholder='Search here...' />
-                        </center>
+
+                    <div className='w-96 flex items-center md:ml-auto'>
+                        <input value={search} onChange={(e) => {
+                            setIspop(true)
+                            setSearch(e.target.value)
+                        }} className='p-2 w-11/12' placeholder='Search here...' />
                     </div>
+
+
                 </div>
 
             </Card>
             <OutsideClickHandler onOutsideClick={(e) => { setIspop(false) }}>
-                {search && ispop && <div className='absolute md:mx-24 w-80 ml-5 md:w-96 h-auto bg-blue-900 text-white z-50 rounded-md'>
-                    {users?.map(obj => {
-                        return (
-                            <div key={obj._id} className='p-3'>
 
-                                <div className='flex  gap-3 cursor-pointer hover:text-yellow-400 rounded-md'>
-                                    <div>
-                                        <Link to='/profile/' state={{ id: obj?._id }}>
-                                            <Avatar url={obj?.profilepic} />
-                                        </Link>
-                                    </div>
-                                    <div>
-                                        <p className=' font-semibold mt-2'>{obj.firstname + " " + obj.lastname}</p>
+                {search && ispop &&
+                    <div className='md:ml-auto -mt-4  md:w-96 h-auto bg-gray-300 text-white z-50 rounded-md'>
+                        {users?.map(obj => {
+                            return (
+                                <div key={obj._id} className='p-3'>
+
+                                    <div className='flex  gap-3 cursor-pointer hover:text-yellow-400 rounded-md'>
+                                        <div>
+                                            <Link to='/profile/' state={{ id: obj?._id }}>
+                                                <Avatar url={obj?.profilepic} />
+                                            </Link>
+                                        </div>
+                                        <div>
+                                            <p className=' font-semibold mt-2'>{obj.firstname + " " + obj.lastname}</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })}
-                </div>}
+                            )
+                        })}
+                    </div>
+                }
             </OutsideClickHandler>
         </div>
 
